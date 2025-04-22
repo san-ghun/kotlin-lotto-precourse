1ff75715456c7a262b0c23a710a73c72b98fdbcb
woowahan-pjs <jason@woowahan.com>
2025-04-22 21:30:05 +0900

    feat: set up the project

---
+++ src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,9 @@
+package lotto
+
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+    }
+
+    // TODO: Implement additional functions
+}
---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,23 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoTest {
+    @Test
+    fun `throws an exception when lotto numbers exceed six`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    // TODO: Implement production code to pass the test
+    @Test
+    fun `throws an exception when lotto numbers contain duplicates`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    // TODO: Implement tests based on the added features
+}
