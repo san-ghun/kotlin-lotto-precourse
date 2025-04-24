020c0a1e744e8e62bc7567d74f5865ee1aedb508
Sanghun <po4865@gmail.com>
2025-04-24 20:50:06 +0200

    chore(InputView): add input validation check for 'readPurchaseAmount()' in comments

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,20 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import net.bytebuddy.pool.TypePool.Resolution.Illegal
 
 object InputView {
     fun readPurchaseAmount(): Int {
         println("Please enter the purchase amount.")
-        return Console.readLine().toIntOrNull()
-            ?: throw IllegalArgumentException("[ERROR] Input must be a number.")
+        val amount = Console.readLine()
+            .trim()
+        // Check non-numeric
+            .toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Input must be a number.")
+        // Check zero
+        // Check negative
+        // Check less than 1000
+        // Check divisible by 1000
+
+        return amount
     }
 }
