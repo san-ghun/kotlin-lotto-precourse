506f250f363c72cad68504c62c792467c18492fa
Sanghun <po4865@gmail.com>
2025-04-24 18:57:29 +0200

    chore(main): draft the overall program flow in comment

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    // 1. Input Purchase Amount
+
+    // 2. Issue Lottery Tickets
+
+    // 3. Input Winning Numbers
+
+    // 4. Calculate Winning Matches
+
+    // 5. Output Statistics and Return Rate
 }
