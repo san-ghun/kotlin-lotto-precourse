ff2f3be96c5ffe06c3b0e7dc64986622be9b83c6
Sanghun <po4865@gmail.com>
2025-04-24 19:22:41 +0200

    chore(main): draft a simple pseudocode for program flow

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,12 +2,26 @@ package lotto
 
 fun main() {
     // 1. Input Purchase Amount
+    //val amount = InputView.readPurchaseAmount()
 
     // 2. Issue Lottery Tickets
+    //val lottoTicket = LottoGenerator.generate(amount)
+    //OutputView.printPurchasedLotto(lottoTicket)
 
     // 3. Input Winning Numbers
+    //val winningNumbers = InputView.readWinningNumbers()
+    //val bonusNumber = Input.readBonusNumber()
+
+    //val winningLotto = WinningLotto(Lotto(winningNumbers), bonusNumber)
 
     // 4. Calculate Winning Matches
+    //val result = LottoResult()
+    //lottoTicket.getTickets().forEach { lotto ->
+    //    val rank = winningLotto.match(lotto)
+    //    result.add(rank)
+    //}
 
     // 5. Output Statistics and Return Rate
+    //OutputView.printStatistics(result)
+    //OutputView.printReturnRate(result, amount)
 }
