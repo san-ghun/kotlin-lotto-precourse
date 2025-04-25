6c62f14fb4a26c333162b3e5454f06c2007b463f
Sanghun <po4865@gmail.com>
2025-04-25 20:04:29 +0200

    test(LottoMachineTest): add test methods for lotto ticket generation in 'issueBundle()'

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -2,11 +2,44 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 
 class LottoMachineTest {
+
+    private val testMachine = LottoMachine()
+
+    @Test
+    fun `issue bundle of lotto tickets based on the amount of input`() {
+        val testCases = listOf(1000, 5000, 3000, 9000, 15000, 98000, 135000)
+        testCases.forEach { case ->
+            val tickets = testMachine.issueBundle(case).tickets
+            assertEquals(case / 1_000, tickets.size)
+        }
+    }
+
+    @Test
+    fun `each lotto tickets have 6 numbers`() {
+        val testCases = listOf(1000, 5000, 3000)
+        testCases.forEach { case ->
+            val firstTicket = testMachine.issueBundle(case).tickets[0]
+            assertEquals(6, firstTicket.getNumbers().size)
+        }
+    }
+
+    @Test
+    fun `lotto tickets have non-duplicated numbers between 1 and 45`() {
+        val testCases = listOf(1000, 2000, 7000)
+        testCases.forEach { case ->
+            val firstTicket = testMachine.issueBundle(case).tickets[0]
+            val numbers = firstTicket.getNumbers()
+            assertTrue(numbers.all { it in 1..45 })
+            assertEquals(6, numbers.distinct().size)
+        }
+    }
+
     @Test
     fun `throws an exception when amount of currency is less than the price of a ticket`() {
-        val testMachine = LottoMachine()
         val testCases = listOf(0, -5, 1, 2, 9, 10, 100, 523, 999)
         testCases.forEach { case ->
             assertThrows<IllegalArgumentException> {
@@ -17,7 +50,6 @@ class LottoMachineTest {
 
     @Test
     fun `throws an exception when amount of currency is indivisible by the price of a ticket`() {
-        val testMachine = LottoMachine()
         val testCases = listOf(1001, 1100, 1010, 1110, 1011, 4564, 9999, 1928376)
         testCases.forEach { case ->
             assertThrows<IllegalArgumentException> {
