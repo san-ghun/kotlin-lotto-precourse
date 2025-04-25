861f25696fa170649d6b94323c6523034a1a23d5
Sanghun <po4865@gmail.com>
2025-04-25 19:20:36 +0200

    chore(LottoMachine): apply 'RandomLottoGenerator'
    - Apply 'RandomLottoGenerator' as default parameter using dependency injection.

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,7 +2,7 @@ package lotto
 
 private const val PRICE_PER_TICKET = 1_000
 
-class LottoMachine(private val generator: LottoGenerator = TODO("RandomLottoGenerator")) {
+class LottoMachine(private val generator: LottoGenerator = RandomLottoGenerator) {
     fun issueBundle(amount: Int): LottoBundle {
         // Calculate number of ticket based on purchase amount
         val count = amount / PRICE_PER_TICKET
