2ba1afa13732555d8ecd596883e1d42c6407e44c
Sanghun <po4865@gmail.com>
2025-04-25 19:43:35 +0200

    test(LottoMachineTest): add failing test methods for input validation of 'issueBundle()'
    - both test methods test multiple test cases for input value of 'issueBundle()' method.

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -6,11 +6,23 @@ import org.junit.jupiter.api.assertThrows
 class LottoMachineTest {
     @Test
     fun `throws an exception when amount of currency is less than the price of a ticket`() {
-
+        val testMachine = LottoMachine()
+        val testCases = listOf(0, -5, 1, 2, 9, 10, 100, 523, 999)
+        testCases.forEach { case ->
+            assertThrows<IllegalArgumentException> {
+                testMachine.issueBundle(case)
+            }
+        }
     }
 
     @Test
     fun `throws an exception when amount of currency is indivisible by the price of a ticket`() {
-
+        val testMachine = LottoMachine()
+        val testCases = listOf(1001, 1100, 1010, 1110, 1011, 4564, 9999, 1928376)
+        testCases.forEach { case ->
+            assertThrows<IllegalArgumentException> {
+                testMachine.issueBundle(case)
+            }
+        }
     }
 }
