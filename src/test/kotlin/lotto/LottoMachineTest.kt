dd7edad96846d2a2fb53f4af22d2a1919e371b44
Sanghun <po4865@gmail.com>
2025-04-25 19:31:31 +0200

    test(LottoMachineTest): add blank test methods
    - test method for 'throws an exception when amount of currency is less than the price of a ticket'
    - test method for 'throws an exception when amount of currency is indivisible by the price of a ticket'

---
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -0,0 +1,16 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoMachineTest {
+    @Test
+    fun `throws an exception when amount of currency is less than the price of a ticket`() {
+
+    }
+
+    @Test
+    fun `throws an exception when amount of currency is indivisible by the price of a ticket`() {
+
+    }
+}
