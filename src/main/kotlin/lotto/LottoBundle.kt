18f6fc78a7b27f233856baa8cc06f65e5ddc4f13
Sanghun <po4865@gmail.com>
2025-04-26 15:03:41 +0200

    chore(LottoBundle): add todo comment for further improvement suggestion

--- src/main/kotlin/lotto/LottoBundle.kt
+++ src/main/kotlin/lotto/LottoBundle.kt
@@ -1,3 +1,5 @@
 package lotto
 
+// TODO: convert to general class only to encapsulate and process list of 'Lotto'
+// There is no need for this class to be a data class.
 data class LottoBundle(val tickets: List<Lotto>)
