760a23c6b5b8fd693919c2d9a62acc1986a87f5f
Sanghun <po4865@gmail.com>
2025-04-26 23:01:35 +0200

    feat(OutputView): add method 'printStatistics()' in pseudocode
    - Have to implement deriving method 'report()' inside of 'LottoResult' class.

--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -1,6 +1,8 @@
 package lotto.ui
 
 import lotto.LottoBundle
+import lotto.LottoResult
+import lotto.Rank
 
 object OutputView {
     fun printPurchasedLotto(bundle: LottoBundle) {
@@ -9,5 +11,20 @@ object OutputView {
         tickets.forEach { ticket ->
             println(ticket.getNumbers().toString())
         }
+    }
+
+    fun printStatistics(result: LottoResult) {
+        val board = result.winningBoard
+        println("\nWinning Statistics")
+        println("---")
+        println(result.report(Rank.entries[4]))
+        println(result.report(Rank.entries[3]))
+        println(result.report(Rank.entries[2]))
+        println(result.report(Rank.entries[1]))
+        println(result.report(Rank.entries[0]))
+    }
+
+    fun printProfitRate(result: LottoResult, amount: Int) {
+        TODO("print profit rate with term 'return rate'")
     }
 }
