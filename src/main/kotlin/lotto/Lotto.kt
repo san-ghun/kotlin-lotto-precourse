48c584851d908aee2485f7732d49a96ca1eeaa61
Sanghun <po4865@gmail.com>
2025-04-26 10:04:17 +0200

    refactor(Lotto): edit 'getNumbers()' method to return sorted copy of 'numbers' as immutable list

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,6 +10,5 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45."}
     }
 
-    //TODO: make it return copy of 'numbers'
-    fun getNumbers() = numbers
+    fun getNumbers(): List<Int> = numbers.sorted()
 }
