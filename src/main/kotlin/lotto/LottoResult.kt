d08c7682f4aa35e7d8ea2bfe7b723715b5e8328a
Sanghun <po4865@gmail.com>
2025-04-26 18:36:22 +0200

    feat(LottoResult): implement 'LottoResult' class to record winning status using property 'winningBoard'
    - Use mutable map with 'Rank' and 'Int' to store record of winning status.

---
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -0,0 +1,15 @@
+package lotto
+
+class LottoResult {
+    private var _winningBoard: MutableMap<Rank, Int> = mutableMapOf()
+    val winningBoard: MutableMap<Rank, Int>
+        get() = _winningBoard
+
+    init {
+        Rank.entries.forEach { _winningBoard[it] = 0 }
+    }
+
+    fun add(rank: Rank) {
+        _winningBoard[rank] = _winningBoard.getOrDefault(rank, 0) + 1
+    }
+}
