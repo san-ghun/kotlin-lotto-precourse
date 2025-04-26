a4f01c7beae9471c5db6ea1d4fd31f2641eb56a2
Sanghun <po4865@gmail.com>
2025-04-26 14:58:45 +0200

    feat(WinningLotto): add and implement 'WinningLotto' class to pass test

---
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -0,0 +1,23 @@
+package lotto
+
+class WinningLotto(
+    private val numbers: List<Int>,
+    private val bonusNumber: Int,
+) {
+
+    init {
+        require(bonusNumber in 1..45) { "[ERROR] Bonus number must be in between 1 and 45." }
+        require(!numbers.contains(bonusNumber)) { "[ERROR] Bonus number must not a duplicated from the winning numbers." }
+    }
+
+    val ticket: Lotto
+        get() = Lotto(numbers)
+
+    fun matchCount(lotto: Lotto): Int {
+        return lotto.getNumbers().count { it in ticket.getNumbers() }
+    }
+
+    fun isMatchBonusNumber(lotto: Lotto): Boolean {
+        return bonusNumber in lotto.getNumbers()
+    }
+}
