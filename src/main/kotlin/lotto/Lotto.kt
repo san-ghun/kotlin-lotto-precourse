b8a35e9822a03682f7565388e08d8ff829325265
Sanghun <po4865@gmail.com>
2025-04-27 17:39:34 +0200

    fix(Lotto): remove instance variables

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,12 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
-    private val lottoLength: Int = Config.LENGTH_OF_LOTTO
-    private val minNumber: Int = Config.MIN_LOTTO_NUMBER
-    private val maxNumber: Int = Config.MAX_LOTTO_NUMBER
-
     init {
-        require(numbers.size == lottoLength) { "[ERROR] Lotto must contain exactly $lottoLength numbers." }
-        require(numbers.distinct().size == lottoLength) { "[ERROR] Lotto numbers must be unique."}
-        require(numbers.all { it in minNumber..maxNumber }) { "[ERROR] Lotto numbers must be between $minNumber and $maxNumber."}
+        require(numbers.size == Config.LENGTH_OF_LOTTO) { "[ERROR] Lotto must contain exactly ${Config.LENGTH_OF_LOTTO} numbers." }
+        require(numbers.distinct().size == Config.LENGTH_OF_LOTTO) { "[ERROR] Lotto numbers must be unique."}
+        require(numbers.all {
+            it in Config.MIN_LOTTO_NUMBER..Config.MAX_LOTTO_NUMBER
+        }) { "[ERROR] Lotto numbers must be between ${Config.MIN_LOTTO_NUMBER} and ${Config.MAX_LOTTO_NUMBER}."}
     }
 
     fun getNumbers(): List<Int> = numbers.sorted()
