6be319608b0129f5f6163c2dea62ca72049b4de4
Sanghun <po4865@gmail.com>
2025-04-27 02:07:11 +0200

    refactor(LottoGenerator): move 'RandomLottoGenerator' object into 'LottoGenerator.kt' file

--- src/main/kotlin/lotto/util/LottoGenerator.kt
+++ src/main/kotlin/lotto/util/LottoGenerator.kt
@@ -1,5 +1,14 @@
 package lotto.util
 
+import camp.nextstep.edu.missionutils.Randoms
+
 interface LottoGenerator {
     fun generateNumbers(): List<Int>
+}
+
+object RandomLottoGenerator: LottoGenerator {
+    override fun generateNumbers(): List<Int> {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return numbers.sorted()
+    }
 }
