cd18278dd93e86772a15cce288169b3cb2119146
Sanghun <po4865@gmail.com>
2025-04-27 16:39:18 +0200

    chore(main): remove comments

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,28 +4,22 @@ import lotto.ui.InputView
 import lotto.ui.OutputView
 
 fun main() {
-    // 1. Input Purchase Amount
     val amount: Int = retryable { InputView.readPurchaseAmount() }
 
-    // 2. Issue Lottery Tickets
     val machine = LottoMachine()
     val lottoBundle: List<Lotto> = machine.issueBundle(amount)
     OutputView.printPurchasedLotto(lottoBundle)
 
-    // 3. Input Winning Numbers
     val winningNumbers: List<Int> = retryable { InputView.readWinningNumbers() }
     val bonusNumber: Int = retryable { InputView.readBonusNumber(winningNumbers) }
-
     val winningLotto = WinningLotto(winningNumbers, bonusNumber)
 
-    // 4. Calculate Winning Matches
     val result = LottoResult()
     lottoBundle.forEach { lotto ->
         val rank = winningLotto.match(lotto)
         result.add(rank)
     }
 
-    // 5. Output Statistics and Return Rate
     OutputView.printStatistics(result)
     OutputView.printProfitRate(result, amount)
 }
