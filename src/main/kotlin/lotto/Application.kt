dbdb100ddb5e4390122be21327b06d26e4ba681e
Sanghun <po4865@gmail.com>
2025-04-27 21:55:10 +0200

    refactor(main): separate 'main()' function into pieces of private functions

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,23 +5,33 @@ import lotto.ui.OutputView
 
 fun main() {
     val amount = retryable { InputView.readPurchaseAmount() }
-
-    val machine = LottoMachine()
-    val lottoBundle = machine.issueBundle(amount)
+    val lottoBundle = issueLottoBundle(amount)
     OutputView.printPurchasedLotto(lottoBundle)
 
+    val winningLotto = takeWinningNumbers()
+    val result = getLottoResult(lottoBundle, winningLotto)
+    OutputView.printStatistics(result)
+    OutputView.printProfitRate(result, amount)
+}
+
+private fun issueLottoBundle(amount: Int): List<Lotto> {
+    val machine = LottoMachine()
+    return machine.issueBundle(amount)
+}
+
+private fun takeWinningNumbers(): WinningLotto {
     val winningNumbers = retryable { InputView.readWinningNumbers() }
     val bonusNumber = retryable { InputView.readBonusNumber(winningNumbers) }
-    val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+    return WinningLotto(winningNumbers, bonusNumber)
+}
 
+private fun getLottoResult(lottoBundle: List<Lotto>, winningLotto: WinningLotto): LottoResult {
     val result = LottoResult()
     lottoBundle.forEach { lotto ->
         val rank = winningLotto.match(lotto)
         result.add(rank)
     }
-
-    OutputView.printStatistics(result)
-    OutputView.printProfitRate(result, amount)
+    return result
 }
 
 private fun <T> retryable(inputMethod: () -> T): T {
