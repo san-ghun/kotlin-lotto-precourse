896f1634c541468c596bd3f9d8c9f7d7528b17c8
Sanghun <po4865@gmail.com>
2025-04-28 19:41:55 +0200

    refactor(LottoMachine): separate operations in 'issueBundle()' into private methods
    - add new methods 'validateAmount()' and 'calculateTicketCount()'.

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -4,14 +4,23 @@ import lotto.util.LottoGenerator
 import lotto.util.RandomLottoGenerator
 
 class LottoMachine(private val generator: LottoGenerator = RandomLottoGenerator) {
+    private val pricePerTicket = Config.PRICE_PER_TICKET
+
     fun issueBundle(amount: Int): List<Lotto> {
-        val pricePerTicket = Config.PRICE_PER_TICKET
-
-        require(amount >= pricePerTicket) { "[ERROR] Amount must be at least ${pricePerTicket}." }
-        require(amount % pricePerTicket == 0) { "[ERROR] Amount must be divisible by ${pricePerTicket}." }
-
-        val count = amount / pricePerTicket
+        val validatedAmount = validateAmount(amount)
+        val count = calculateTicketCount(validatedAmount)
         val bundle = List(count) { Lotto(generator.generateNumbers()) }
         return bundle
     }
+
+    private fun validateAmount(amount: Int): Int {
+        require(amount >= pricePerTicket) { "[ERROR] Amount must be at least ${pricePerTicket}." }
+        require(amount % pricePerTicket == 0) { "[ERROR] Amount must be divisible by ${pricePerTicket}." }
+        return amount
+    }
+
+    private fun calculateTicketCount(amount: Int): Int {
+        return amount / pricePerTicket
+    }
+
 }
