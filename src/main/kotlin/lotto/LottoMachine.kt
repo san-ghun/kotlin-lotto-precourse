e64d8f7bb395410a85c0e936e0d490ca5c202f27
Sanghun <po4865@gmail.com>
2025-05-05 17:01:46 +0200

    feat(LottoMachine): update 'LottoMachine' to record winner ranks for lotto tickets

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -5,6 +5,28 @@ class LottoMachine(
     private val winningTicket: Lotto,
     private val bonusNumber: Int,
 ) {
+    private val _winnerBoard = mutableMapOf<Rank, Int>()
+
+    val winnerBoard: Map<Rank, Int>
+        get() = _winnerBoard
+
+    init {
+        Rank.entries.forEach { _winnerBoard[it] = 0 }
+        matchWinning()
+    }
+
+    private fun addWinning(rank: Rank) {
+        _winnerBoard[rank] = _winnerBoard.getOrDefault(rank, 0) + 1
+    }
+
+    private fun matchWinning() {
+        lottoBundle.forEach { ticket ->
+            val matchCount = ticket.getNumbers().count { it in winningTicket.getNumbers() }
+            val isMatchBonus = bonusNumber in ticket.getNumbers()
+            addWinning(Rank.matchRank(matchCount, isMatchBonus))
+        }
+    }
+
     companion object {
         fun purchaseTickets(amount: Int): List<Lotto> {
             val howManyTickets = amount / 1_000
