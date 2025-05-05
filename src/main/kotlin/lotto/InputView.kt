612c97000eeca9d291c202db0a2be20b66b4f569
Sanghun <po4865@gmail.com>
2025-05-05 14:45:45 +0200

    feat(InputView): add new method 'readWinningNumbers()'

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -11,4 +11,17 @@ object InputView {
         require(amount % 1_000 == 0) { "[ERROR] Invalid input - indivisible by 1,000, read purchase amount" }
         return amount
     }
+
+    fun readWinningNumbers(): List<Int> {
+        val userInput = Console.readLine().trim()
+        require(userInput.isNotBlank()) { "[ERROR] Invalid input - is blank, read winning numbers" }
+        val splitInput = userInput.split(",")
+        require(splitInput.size == 6) { "[ERROR] Invalid input - there must be 6 numbers, read winning numbers" }
+        val numbers = splitInput.map {
+            it.toIntOrNull()
+                ?: throw IllegalArgumentException("[ERROR] Invalid input - there must be 6 numbers, read winning numbers")
+        }
+        require(numbers.distinct().size == 6) { "[ERROR] Invalid input - there must be 6 numbers, read winning numbers" }
+        return numbers.sorted()
+    }
 }
