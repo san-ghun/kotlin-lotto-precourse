74d33942ec9ae04b9b61967594fa51413cb681cc
Sanghun <po4865@gmail.com>
2025-05-05 17:05:27 +0200

    feat(Application): add generic method 'reprompt()' to to reprompt user input when it fail to return correctly

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,27 +2,30 @@ package lotto
 
 fun main() {
     println("Please enter the purchase amount.")
-    val purchasingAmount = InputView.readPurchaseAmount()
+    val purchasingAmount = reprompt { InputView.readPurchaseAmount() }
     val lottoBundle = LottoMachine.purchaseTickets(purchasingAmount)
 
     println("\nYou have purchased ${lottoBundle.size} tickets.")
     OutputView.showTickets(lottoBundle)
 
     println("\nPlease enter the bonus number.")
-    val winningTicket = InputView.readWinningNumbers()
+    val winningNumbers = reprompt { InputView.readWinningNumbers() }
+    val winningTicket = Lotto(winningNumbers)
 
     println("\nPlease enter the bonus number.")
-    val bonusNumber = InputView.readBonusNumber()
+    val bonusNumber = reprompt { InputView.readBonusNumber(winningNumbers) }
 
     val machine = LottoMachine(lottoBundle, winningTicket, bonusNumber)
 
-//    OutputView.showStatistics(machine)
-    println("\nWinning Statistics")
-    println("---")
-    println("3 Matches (0,000 KRW) - 0 tickets")
-    println("4 Matches (0,000 KRW) - 0 tickets")
-    println("5 Matches (0,000 KRW) - 0 tickets")
-    println("5 Matches + Bonus Ball (0,000 KRW) - 0 tickets")
-    println("6 Matches (0,000 KRW) - 0 tickets")
-    println("Total return rate is 00.0%.")
+    OutputView.showStatistics(machine)
+}
+
+private fun <T> reprompt(method: () -> T): T {
+    while (true) {
+        try {
+            return method()
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+        }
+    }
 }
