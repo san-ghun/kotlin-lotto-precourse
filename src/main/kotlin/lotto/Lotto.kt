297ff3533cbdfa780959a0c0d11265fe377c0e43
Sanghun <po4865@gmail.com>
2025-05-05 14:23:31 +0200

    feat(Lotto): add validation processes in initializer and add getter method for 'numbers'

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,9 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Lotto must consist of 6 unique numbers." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be in the range from 1 to 45." }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers() = numbers.sorted()
 }
