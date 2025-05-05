6e4ed4a84438f6c04391cf0cc61529c05a6bf385
Sanghun <po4865@gmail.com>
2025-05-05 13:52:53 +0200

    chore(main): sketch 'main()' function

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,28 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    println("Please enter the purchase amount.")
+    val purchasingAmount = InputView.readPurchaseAmount()
+    val lottoBundle = LottoMachine.purchaseTickets(purchasingAmount)
+
+    println("\nYou have purchased ${lottoBundle.size} tickets.")
+    OutputView.showTickets(lottoBundle)
+
+    println("\nPlease enter the bonus number.")
+    val winningNumbers = InputView.readWinningNumbers()
+
+    println("\nPlease enter the bonus number.")
+    val bonusNumber = InputView.readBonusNumber()
+
+    val machine = LottoMachine(lottoBundle, winningNumbers, bonusNumber)
+
+//    OutputView.showStatistics(machine)
+    println("\nWinning Statistics")
+    println("---")
+    println("3 Matches (0,000 KRW) - 0 tickets")
+    println("4 Matches (0,000 KRW) - 0 tickets")
+    println("5 Matches (0,000 KRW) - 0 tickets")
+    println("5 Matches + Bonus Ball (0,000 KRW) - 0 tickets")
+    println("6 Matches (0,000 KRW) - 0 tickets")
+    println("Total return rate is 00.0%.")
 }
