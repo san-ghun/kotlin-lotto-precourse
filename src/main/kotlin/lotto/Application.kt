48a17de861f52acdb400672d1f3db424d39b5d6f
Sanghun <po4865@gmail.com>
2025-05-05 17:15:23 +0200

    style(Application): separate 'main()' into smaller functions

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,23 +1,41 @@
 package lotto
 
+import javax.accessibility.AccessibleBundle
+
 fun main() {
+    val lottoBundle = takeLottoTickets()
+    issueLottoTickets(lottoBundle)
+
+    val winningNumbers = takeWinningNumber()
+    val winningTicket = Lotto(winningNumbers)
+    val bonusNumber = takeBonusNumber(winningNumbers)
+
+    val machine = LottoMachine(lottoBundle, winningTicket, bonusNumber)
+    OutputView.showStatistics(machine)
+}
+
+private fun takeLottoTickets(): List<Lotto> {
     println("Please enter the purchase amount.")
     val purchasingAmount = reprompt { InputView.readPurchaseAmount() }
     val lottoBundle = LottoMachine.purchaseTickets(purchasingAmount)
+    return lottoBundle
+}
 
+private fun issueLottoTickets(lottoBundle: List<Lotto>) {
     println("\nYou have purchased ${lottoBundle.size} tickets.")
     OutputView.showTickets(lottoBundle)
+}
 
+private fun takeWinningNumber(): List<Int> {
     println("\nPlease enter the bonus number.")
     val winningNumbers = reprompt { InputView.readWinningNumbers() }
-    val winningTicket = Lotto(winningNumbers)
+    return winningNumbers
+}
 
+private fun takeBonusNumber(winningNumbers: List<Int>): Int {
     println("\nPlease enter the bonus number.")
     val bonusNumber = reprompt { InputView.readBonusNumber(winningNumbers) }
-
-    val machine = LottoMachine(lottoBundle, winningTicket, bonusNumber)
-
-    OutputView.showStatistics(machine)
+    return bonusNumber
 }
 
 private fun <T> reprompt(method: () -> T): T {
