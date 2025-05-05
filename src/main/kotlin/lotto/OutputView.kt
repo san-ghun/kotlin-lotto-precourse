0122b195a8774effab2dcf3b8f5d723ef951b1b5
Sanghun <po4865@gmail.com>
2025-05-05 17:02:59 +0200

    feat(OutputView): add method 'showStatistics()' to show winner result and total return rate

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,10 +1,25 @@
 package lotto
 
+import java.util.Locale
+
 object OutputView {
 
     fun showTickets(lottoBundle: List<Lotto>) {
         lottoBundle.forEach { ticket ->
             println(ticket.getNumbers())
         }
+    }
+
+    fun showStatistics(lottoMachine: LottoMachine) {
+        val ranks = Rank.entries
+        val winnerBoard = lottoMachine.winnerBoard
+        println("\nWinning Statistics")
+        println("---")
+        println("${ranks[4].matchCount} Matches (${String.format(Locale.US, "%,d", ranks[4].prize)} KRW) – ${winnerBoard[ranks[4]]} tickets")
+        println("${ranks[3].matchCount} Matches (${String.format(Locale.US, "%,d", ranks[3].prize)} KRW) – ${winnerBoard[ranks[3]]} tickets")
+        println("${ranks[2].matchCount} Matches (${String.format(Locale.US, "%,d", ranks[2].prize)} KRW) – ${winnerBoard[ranks[2]]} tickets")
+        println("${ranks[1].matchCount} Matches + Bonus Ball (${String.format(Locale.US, "%,d", ranks[1].prize)} KRW) – ${winnerBoard[ranks[1]]} tickets")
+        println("${ranks[0].matchCount} Matches (${String.format(Locale.US, "%,d", ranks[0].prize)} KRW) – ${winnerBoard[ranks[0]]} tickets")
+        println("Total return rate is ${lottoMachine.calculateReturnRate()}%.")
     }
 }
